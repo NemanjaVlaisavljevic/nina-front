9eb26b06a524a17daa30a8d5a583e0162b09d9d3
Nikola Stojšin <nikola.stojsin@hotmail.com>
2021-03-06 08:11:35 +0100

    Updated dependecies

--- nina-front/src/app/parts/product-card-deck/product-card-deck.component.spec.ts
+++ nina-front/src/app/parts/product-card-deck/product-card-deck.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ProductCardDeckComponent } from './product-card-deck.component';
 
@@ -6,7 +6,7 @@ describe('ProductCardDeckComponent', () => {
   let component: ProductCardDeckComponent;
   let fixture: ComponentFixture<ProductCardDeckComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ProductCardDeckComponent ]
     })
